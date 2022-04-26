bf931584834c3fc833bead0bf00397888cec9590
Seweryn Pokutycki <ksernosikosik@wp.pl>
2022-04-27 01:18:29 +0200

    Filling up the board with tiles, created TileDataSO

---
+++ Assets/Scripts/TileDataSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Tile Data", menuName = "New Tile Data/Tile Data")]
+public class TileDataSO : ScriptableObject
+{
+    public Sprite WhiteSprite;
+    public Sprite BlackSprite;
+}
---
+++ Assets/Scripts/TileState.cs
@@ -0,0 +1,5 @@
+public enum TileState
+{
+    White,
+    Black
+}
