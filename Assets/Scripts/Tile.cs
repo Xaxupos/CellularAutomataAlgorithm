b24fb9d89fbb2cece085978fe8a4fcb3dbee7702
Seweryn Pokutycki <ksernosikosik@wp.pl>
2022-04-27 02:35:19 +0200

    Assigning neighbors to each tile

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,7 +1,48 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Image))]
 public class Tile : MonoBehaviour
 {
     [SerializeField] private TileState _tileState;
     [SerializeField] private TileDataSO _tileData;
+    [SerializeField] private Vector2 _gridPosition;
+
+    [SerializeField] private List<Tile> _neighborTiles = new List<Tile>();
+
+    private Image _tileImage;
+
+    private void Awake()
+    {
+        _tileImage = GetComponent<Image>();
+    }
+
+    private void Start()
+    {
+        SetIntialValues();
+    }
+
+    public Vector2 GetGridPosition()
+    {
+        return _gridPosition;
+    }
+
+    public void SetGridPosition(Vector2 pos)
+    {
+        _gridPosition = pos;
+    }
+
+    public void AddNeighborTile(Tile tile)
+    {
+        if(_neighborTiles.Contains(tile)) return;
+
+        _neighborTiles.Add(tile);
+    }
+
+    private void SetIntialValues()
+    {
+        _tileImage.sprite = _tileData.BlackSprite;
+        _tileState = TileState.Black;
+    }
 }
---
+++ Assets/Scripts/TileFiller.cs
@@ -0,0 +1,108 @@
+using UnityEngine;
+
+public class TileFiller : MonoBehaviour
+{
+    [SerializeField] private GameObject _tilePrefab;
+
+    private RectTransform _rectTransform;
+    private Tile[,] _tileArray;
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void Start()
+    {
+        FillBoardWithTiles();
+    }
+
+    private void FillBoardWithTiles()
+    {
+        float width = _rectTransform.sizeDelta.x;
+        float height = _rectTransform.sizeDelta.y;
+
+        _tileArray = new Tile[(int)width, (int)height];
+
+        int columns = (int)width / 10;
+        int rows = (int)height / 10;
+
+        for (int x = 0; x < rows; x++)
+        {
+            for (int y = 0; y < columns; y++)
+            {
+                var tile = Instantiate(_tilePrefab, this.transform);
+                _tileArray[x, y] = tile.GetComponent<Tile>();
+                tile.GetComponent<Tile>().SetGridPosition(new Vector2(x, y));
+            }
+        }
+
+        AssignNeighbors(columns, rows, _tileArray);
+    }
+
+    private static void AssignNeighbors(int columns, int rows, Tile[,] tileArray)
+    {
+        for (int x = 0; x < rows; x++)
+        {
+            for (int y = 0; y < columns; y++)
+            {   
+                if(y < columns-1 && tileArray[x,y+1].GetGridPosition().x == tileArray[x, y].GetGridPosition().x)
+                {
+                    if (tileArray[x, y+1].GetGridPosition().y-1 == tileArray[x, y].GetGridPosition().y)
+                    {
+                        tileArray[x, y].AddNeighborTile(tileArray[x, y+1]);
+                    }
+                }
+                if(y>0 && tileArray[x,y-1].GetGridPosition().x == tileArray[x, y].GetGridPosition().x)
+                {
+                    if (tileArray[x, y-1].GetGridPosition().y+1 == tileArray[x, y].GetGridPosition().y)
+                    {
+                        tileArray[x, y].AddNeighborTile(tileArray[x, y-1]);
+                    }
+                }
+                if(x>0 && tileArray[x-1, y].GetGridPosition().y == tileArray[x, y].GetGridPosition().y)
+                {
+                    if (tileArray[x-1, y].GetGridPosition().x + 1 == tileArray[x, y].GetGridPosition().x)
+                    {
+                        tileArray[x, y].AddNeighborTile(tileArray[x-1, y]);
+                    }
+                }
+                if(x < rows-1 && tileArray[x+1, y].GetGridPosition().y == tileArray[x, y].GetGridPosition().y)
+                {
+                    if (tileArray[x+1, y].GetGridPosition().x - 1 == tileArray[x, y].GetGridPosition().x)
+                    {
+                        tileArray[x, y].AddNeighborTile(tileArray[x+1, y]);
+                    }
+                }
+                if(x>0 && y>0 && tileArray[x-1,y-1].GetGridPosition().x+1 == tileArray[x, y].GetGridPosition().x)
+                {
+                    if(tileArray[x - 1, y - 1].GetGridPosition().y + 1 == tileArray[x, y].GetGridPosition().y)
+                    {
+                        tileArray[x, y].AddNeighborTile(tileArray[x-1, y-1]);
+                    }
+                }
+                if(y>0 && x<rows-1 && tileArray[x+1,y-1].GetGridPosition().x-1 == tileArray[x, y].GetGridPosition().x)
+                {
+                    if(tileArray[x + 1, y - 1].GetGridPosition().y + 1 == tileArray[x, y].GetGridPosition().y)
+                    {
+                        tileArray[x, y].AddNeighborTile(tileArray[x+1, y-1]);
+                    }
+                }
+                if(x>0 && y<columns-1 && tileArray[x-1,y+1].GetGridPosition().x+1 == tileArray[x, y].GetGridPosition().x)
+                {
+                    if(tileArray[x - 1, y + 1].GetGridPosition().y - 1 == tileArray[x, y].GetGridPosition().y)
+                    {
+                        tileArray[x, y].AddNeighborTile(tileArray[x - 1, y + 1]);
+                    }
+                }
+                if(y<columns-1 && x<rows-1 && tileArray[x+1,y+1].GetGridPosition().x-1 == tileArray[x, y].GetGridPosition().x)
+                {
+                    if(tileArray[x + 1, y + 1].GetGridPosition().y - 1 == tileArray[x, y].GetGridPosition().y)
+                    {
+                        tileArray[x, y].AddNeighborTile(tileArray[x + 1, y + 1]);
+                    }
+                }
+            }
+        }
+    }
+}
